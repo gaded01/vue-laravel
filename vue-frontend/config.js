180c252ff885b0ea2e8dbdcf82de75791726786f
Jade <gjcaldabasab1010@gmail.com>
2023-02-24 23:05:18 +0800

    latest commit

---
+++ vue-frontend/config.js
@@ -0,0 +1,3 @@
+const base_route_api = "http://localhost:8000/";
+
+export {base_route_api}
--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -15,14 +15,9 @@ import store from './store/index.js';
 // Plugins
 import { registerPlugins } from '@/plugins'
 
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
-
 const app = createApp(App)
 
 registerPlugins(app)
-
-app.component('VueDatePicker', VueDatePicker);
 
 app.use(store)
 app.mount('#app')
